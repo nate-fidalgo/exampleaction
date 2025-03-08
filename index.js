fe975a3a12ea94e21cda5bc5d26770311292badd
nate-fidalgo <71114853+nate-fidalgo@users.noreply.github.com>
2025-03-07 19:38:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-    const core = require('@actions/core');
+console.log('In my marketplace action');
 
-    try {
-      const inputValue = core.getInput('input_value');
-      console.log(`Input value: ${inputValue}`);
-
-      core.setOutput('output_value', inputValue);
-    } catch (error) {
-      core.setFailed(error.message);
-    }
