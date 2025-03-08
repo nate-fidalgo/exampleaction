f871781c9f69728b75e3002bfa200cd7577dda20
nate-fidalgo <71114853+nate-fidalgo@users.noreply.github.com>
2025-03-07 20:12:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log('In my marketplace action');
+console.log('My input i sent was ${{ inputs.input_value }}');
+//call code in another js file aka morecode.js
+const myModule = require('./morecode');
+
+const result = myModule.printmore(3);
+console.log(result); 
+console.log(myModule.myVariable); // Output: 10
 
